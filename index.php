7ed1082797a4443990560b811bd5b603118bf302
SakamotoAyano <SakamotoAyano@gmail.com>
2020-03-28 14:48:31 +0900

    HTMLで画面を作った

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
-require_once(__DIR__. '/config.php');
-require_once(__DIR__. '/functions.php');
-
+    require_once(__DIR__. '/config.php');
+    require_once(__DIR__. '/functions.php');
 ?>
 
 <!DOCTYPE html>
